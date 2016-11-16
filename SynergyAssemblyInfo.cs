80ffa3d912a70f9640d0436182343d93669233d1
marcin.celej <marcin.celej@gmail.com>
2016-11-16 20:17:43 +0100

    Added empty solution

---
+++ SynergyAssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("Synergy Marcin Celej")]
+[assembly: AssemblyCopyright("Copyright © Synergy Marcin Celej 2016")]
+[assembly: AssemblyProduct("Synergy Framework")]
+[assembly: AssemblyTrademark("Synergy Framework")]
+[assembly: AssemblyConfiguration("")]
+[assembly: ComVisible(false)]
+[assembly: NeutralResourcesLanguage("en")]
