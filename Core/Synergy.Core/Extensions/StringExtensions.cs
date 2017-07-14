3e080e4b25e19af7889516f76433f16aa790ee5e
Marcin Celej <marcin.celej@gmail.com>
2017-07-14 08:38:46 +0200

    I added StringExtensions class

---
+++ Core/Synergy.Core/Extensions/StringExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using JetBrains.Annotations;
+using Synergy.Contracts;
+
+namespace Synergy.Core.Extensions
+{
+    public static class StringExtensions
+    {
+        [Pure]
+        [ContractAnnotation("null=>true")]
+        public static bool IsNullOrEmpty([CanBeNull] this string text)
+        {
+            return String.IsNullOrEmpty(text);
+        }
+
+        [Pure]
+        [ContractAnnotation("null=>true")]
+        public static bool IsNullOrWhiteSpace([CanBeNull] this string text)
+        {
+            return String.IsNullOrWhiteSpace(text);
+        }
+
+        [NotNull, Pure]
+        [ContractAnnotation("text:null=>halt")]
+        public static string Left([NotNull] this string text, int length)
+        {
+            Fail.IfArgumentNull(text, nameof(text));
+
+            if (text.Length <= length)
+                return text;
+
+            return text.Substring(0, length);
+        }
+
+        [CanBeNull, Pure]
+        [ContractAnnotation("text:null=>null")]
+        public static string TrimOrNull([CanBeNull] this string text)
+        {
+            if (String.IsNullOrEmpty(text))
+                return text;
+
+            return text.Trim();
+        }
+    }
+}
