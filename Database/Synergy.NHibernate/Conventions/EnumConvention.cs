d0a2bd2f4fbf11370e5104e0ce9937f950ae7662
Marcin Celej <marcin.celej@gmail.com>
2017-12-11 13:37:31 +0100

    I added EnumConvention

---
+++ Database/Synergy.NHibernate/Conventions/EnumConvention.cs
@@ -0,0 +1,22 @@
+﻿using FluentNHibernate.Conventions;
+using FluentNHibernate.Conventions.AcceptanceCriteria;
+using FluentNHibernate.Conventions.Inspections;
+using FluentNHibernate.Conventions.Instances;
+using JetBrains.Annotations;
+
+namespace Synergy.NHibernate.Conventions
+{
+    [UsedImplicitly(ImplicitUseKindFlags.InstantiatedNoFixedConstructorSignature)]
+    public class EnumConvention : IUserTypeConvention
+    {
+        public void Accept([NotNull] IAcceptanceCriteria<IPropertyInspector> criteria)
+        {
+            criteria.Expect(e => e.Property.PropertyType.IsEnum);
+        }
+
+        public void Apply([NotNull] IPropertyInstance instance)
+        {
+            instance.CustomType(instance.Property.PropertyType);
+        }
+    }
+}
