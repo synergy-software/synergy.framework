64e3a83a42c70750e146e73ad4d0d218271cae45
Cezary Piatek <cezarypiatek@live.com>
2018-08-27 23:49:25 +0200

    Add implementation of WebContextStorage

---
+++ Database/Synergy.NHibernate.AspCore/!Init/SynergyNHibernateAspCore.cs
@@ -0,0 +1,12 @@
+﻿using Synergy.Core;
+
+namespace Synergy.NHibernate.AspCore._Init
+{
+    public class SynergyNHibernateAspCore:Library
+    {
+        /// <inheritdoc />
+        public SynergyNHibernateAspCore() : base(new SynergyNHibernateLibrary())
+        {
+        }
+    }
+}
