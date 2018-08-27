f328583788aed9ef6ca97682b5d922502db84ef9
Cezary Piatek <cezarypiatek@live.com>
2018-08-27 23:49:25 +0200

    Add implementation of WebContextStorage

---
+++ Database/Synergy.NHibernate/Contexts/WebContextStorage.cs
@@ -0,0 +1,10 @@
+﻿namespace Synergy.NHibernate.Contexts
+{
+
+    /// <summary>
+    /// Contextual storage that stores object in a web context.
+    /// </summary>
+    public interface IWebContextStorage<T> : IContextStorage<T>
+    {
+    }
+}
