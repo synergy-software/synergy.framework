d7a1d4ee665354cffa7d23976b548eb68873646d
Marcin Celej <marcin.celej@gmail.com>
2019-04-26 13:50:24 +0200

    I removed the NH.Sample project

--- Database/Synergy.NHibernate.Test/!Init/SynergyNHibernateTestLibrary.cs
+++ Database/Synergy.NHibernate.Test/!Init/SynergyNHibernateTestLibrary.cs
@@ -1,12 +1,10 @@
 ﻿using Synergy.Core;
-using Synergy.NHibernate.Sample;
 
 namespace Synergy.NHibernate.Test
 {
     public class SynergyNHibernateTestLibrary : Library
     {
         public SynergyNHibernateTestLibrary() : base(
-            new SynergyNHibernateSampleLibrary(),
             new SynergyNHibernateLibrary()
             )
         {
---
+++ Database/Synergy.NHibernate.Test/Database/SampleDatabase.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using JetBrains.Annotations;
+using NHibernate.Cfg;
+using Synergy.NHibernate.Engine;
+using Synergy.NHibernate.Test.Database.Users;
+using Synergy.NHibernate.Test.Database.Words;
+
+namespace Synergy.NHibernate.Test.Database
+{
+    [UsedImplicitly(ImplicitUseKindFlags.InstantiatedNoFixedConstructorSignature)]
+    public class SampleDatabase : NHibernate.Engine.Database, ISampleDatabase
+    {
+        public const string SchemaName = "words";
+
+        protected override Configuration GetConfiguration()
+        {
+            Assembly currentAssembly = this.GetType()
+                                           .Assembly;
+            
+            Configuration configure = new Configuration().Configure(currentAssembly, "Synergy.NHibernate.Sample.NHibernate.config");
+            return configure;
+        }
+
+        public override IEnumerable<Type> GetEntities()
+        {
+            return new[]
+            {
+                typeof(User),
+                typeof(WordGroup),
+                typeof(Word)
+            };
+        }
+    }
+
+    public interface ISampleDatabase : IDatabase
+    {
+    }
+}
