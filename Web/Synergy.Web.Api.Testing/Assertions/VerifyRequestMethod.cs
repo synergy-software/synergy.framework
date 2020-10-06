64713f2c7524536a73a22cbf427d531aefb8daeb
MarcinCelej <marcin.celej@gmail.com>
2020-10-06 08:50:46 +0200

    I added ability to chceck multiple Httpo methods at once in VerifyRequestMethod assertion

--- Web/Synergy.Web.Api.Testing/Assertions/VerifyRequestMethod.cs
+++ Web/Synergy.Web.Api.Testing/Assertions/VerifyRequestMethod.cs
@@ -1,27 +1,32 @@
-﻿using System.Net.Http;
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using Synergy.Contracts;
 
 namespace Synergy.Web.Api.Testing.Assertions
 {
     public class VerifyRequestMethod : Assertion
     {
-        private readonly HttpMethod _expectedMethod;
+        private readonly HttpMethod[] _expectedMethod;
 
-        public VerifyRequestMethod(HttpMethod expectedMethod)
+        public VerifyRequestMethod(params HttpMethod[] expectedMethod)
         {
-            _expectedMethod = expectedMethod;
+            _expectedMethod = expectedMethod.OrFail(nameof(expectedMethod));
             ExpectedResult = $"HTTP request method is {_expectedMethod}";
         }
 
         public override Result Assert(HttpOperation operation)
         {
             var actualMethod = operation.Request.Method;
-            if (actualMethod == _expectedMethod)
+            if (_expectedMethod.Contains(actualMethod))
             {
                 return Ok;
             }
 
+            var methods = String.Join(", ", _expectedMethod.Select(m=>m.ToString()));
+
             return Failure(
-                $"Expected HTTP method is {_expectedMethod} but was {actualMethod} " +
+                $"Expected HTTP method is ({methods}) but was {actualMethod} " +
                 $"in request: \n\n{operation.Request.ToHttpLook()}");
         }
     }
