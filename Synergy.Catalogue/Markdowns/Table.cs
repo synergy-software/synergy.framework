74d9e5b664bcdab3150d239d033d9a489606156b
Marcin Celej <marcin.celej@gmail.com>
2022-01-26 07:34:45 +0100

    I added Synergy.Catalogue project - with code for future usage - i'm cataloguing some ideas

---
+++ Synergy.Catalogue/Markdowns/Table.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace Synergy.Catalogue.Markdowns
+{
+    public class Markdown
+    {
+        public class Table
+        {
+            private readonly string[] headers;
+            private readonly List<string[]> rows;
+
+            public Table(params string[] headers)
+            {
+                this.headers = headers;
+                this.rows = new List<string[]>();
+            }
+
+            public void Append(params string[] cells)
+            {
+                this.rows.Add(cells);
+            }
+
+            public override string ToString()
+            {
+                var table = new StringBuilder();
+                foreach (var header in this.headers)
+                {
+                    table.Append($"| {header.Trim()} ");
+                }
+                table.AppendLine("|");
+            
+                foreach (var header in this.headers)
+                {
+                    table.Append($"|{new string('-', header.Length + 2)}");
+                }
+                table.AppendLine("|");
+
+                foreach (var row in this.rows)
+                {
+                    foreach (var cell in row)
+                    {
+                        table.Append($"| {cell?.Trim()} ");
+                    }
+                    table.AppendLine("|");
+                }
+            
+                return table.ToString();
+            }
+        }
+    }
+}
