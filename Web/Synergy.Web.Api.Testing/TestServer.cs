e60bc92c7a4a400a6ec9f5efaf12abbb521183eb
Marcin Celej <marcin.celej@gmail.com>
2022-01-26 13:38:24 +0100

    I added net5.0

--- Web/Synergy.Web.Api.Testing/TestServer.cs
+++ Web/Synergy.Web.Api.Testing/TestServer.cs
@@ -3,7 +3,6 @@ using System.Diagnostics;
 using System.Net.Http;
 using System.Net.Mime;
 using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 using JetBrains.Annotations;
 using Newtonsoft.Json;
