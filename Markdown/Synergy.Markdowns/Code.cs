0ff4e8ce82b2e5fc442747128bf89f5320c91392
Marcin Celej <marcin.celej@gmail.com>
2022-02-04 07:35:07 +0100

    #documentation I added ability to Markdown.Code to start with empty code text and then use Line(string) method

--- Markdown/Synergy.Markdowns/Code.cs
+++ Markdown/Synergy.Markdowns/Code.cs
@@ -1,24 +1,23 @@
-﻿using Synergy.Contracts;
-
-namespace Synergy.Markdowns
+﻿namespace Synergy.Markdowns
 {
     public partial class Markdown
     {
         public class Code : IElement
         {
             private readonly string? language;
-            private readonly string text;
+            private readonly string? text;
 
-            public Code(string text, string? language = "csharp")
+            public Code(string? text = null, string? language = "csharp")
             {
                 this.language = language?.Trim();
-                this.text = text.OrFail(nameof(text))
-                                .Trim();
+                this.text = text;
             }
 
             public Code Line(string line)
             {
-                return new Code(this.text + Markdown.NL + line);
+                if (this.text == null)
+                    return new Code(line, this.language);
+                return new Code(this.text + Markdown.NL + line, this.language);
             }
 
             public override string ToString() => $"``` {this.language ?? ""}{Markdown.NL}" +
