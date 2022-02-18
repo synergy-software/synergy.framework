d3e65d10e8078a95d7bc1e3e2afcf22db281448c
Marcin Celej <marcin.celej@gmail.com>
2022-02-18 16:57:02 +0100

    #16: I added some sample convention rules for convention #testing

---
+++ Contracts/Synergy.Convention.Testing/Rules/SealedRule.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Synergy.Convention.Testing.Rules
+{
+    public static class SealedRule
+    {
+        public static IEnumerable<Deficit> MustBeSealed(this Type type)
+        {
+            if (type.IsSealed == false)
+                yield return new Deficit(type, "sealed");
+        }
+
+        public static IEnumerable<Deficit> MustBeSealedOrAbstract(this Type type)
+        {
+            if (type.IsAbstract)
+                yield break;
+
+            foreach (var deficit in type.MustBeSealed())
+                yield return deficit;
+        }
+    }
+}
