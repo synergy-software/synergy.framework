59b3ab16d3f39c678f5e8c59bc6dbb48aa3004f0
Marcin Celej <marcin.celej@gmail.com>
2023-05-03 08:05:38 +0200

    #23: Added logic that creates empty feature file when there is no such file

---
+++ Behaviours/Synergy.Behaviours.Tests/Samples/New.Steps.cs
@@ -0,0 +1,20 @@
+﻿using Synergy.Behaviours.Testing;
+
+namespace Synergy.Behaviours.Tests.Samples;
+
+[UsesVerify]
+public partial class NewFeature : Feature<NewFeature>
+{
+    [Fact]
+    public async Task GenerateFeature()
+    {
+        var code = this.Generate(
+            from: "New.feature"
+        );
+
+        await Verifier
+              .Verify(code, "cs")
+              .UseFileName("New.Feature")
+              .AutoVerify();
+    }
+}
---
+++ Behaviours/Synergy.Behaviours.Tests/Samples/New.feature
@@ -0,0 +1,10 @@
+Feature: New
+
+# TODO: Provide scenarios here, check the sample down here
+
+#  Scenario: There can be only one
+#    Given there are 3 ninjas
+#    And there are more than one ninja alive
+#    When Two ninjas meet, they will fight
+#    Then one ninja dies (but not me)
+#    And there is one ninja less alive
