bebb90b5a19b71d9f89ea247ce546c391a03e1a8
Marcin Celej <marcin.celej@gmail.com>
2023-05-10 20:23:34 +0200

    #23: Extended Scenario class with additional IsTagged() method

--- Behaviours/Synergy.Behaviours.Testing/Scenario.cs
+++ Behaviours/Synergy.Behaviours.Testing/Scenario.cs
@@ -4,9 +4,18 @@ namespace Synergy.Behaviours.Testing;
 
 public record Scenario(string Title, ReadOnlyCollection<string> Tags)
 {
-    public string Method 
+    public string Method
         => FeatureGenerator.ToMethod(this.Title);
-
-    public bool IsTagged(string tag)
-        => this.Tags.Any(t => t.TrimStart('@').Equals(tag.TrimStart('@'), StringComparison.InvariantCultureIgnoreCase));
+    
+    public bool IsTagged(string tag, params string[] tags)
+    {
+        if (this.IsTagged(tag))
+            return true;
+        
+        return tags.Any(t => this.IsTagged(t));
+    }
+    
+    private bool IsTagged(string tag)
+        => this.Tags.Any(t => t.TrimStart('@')
+                               .Equals(tag.TrimStart('@'), StringComparison.InvariantCultureIgnoreCase));
 }
