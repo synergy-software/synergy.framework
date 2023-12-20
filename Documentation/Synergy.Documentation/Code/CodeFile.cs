4383142d5e75440e9005205404752ba5191ca46e
Marcin Celej <marcin.celej@gmail.com>
2023-12-20 21:25:24 +0100

    #27: Refactor and update CodeFile logic
    The wrong usage of "this" has been corrected, and filePath is now explicitly passed to OrFailIfWhiteSpace for error throw purposes. Invalid path argument exception mechanism is also introduced in Folder property to handle exceptional cases more effectively.

--- Documentation/Synergy.Documentation/Code/CodeFile.cs
+++ Documentation/Synergy.Documentation/Code/CodeFile.cs
@@ -11,24 +11,27 @@ public class CodeFile
         => new(path);
     
     public string FilePath { get; }
-    public string FileName => Path.GetFileName(this.FilePath);
-    public string FileNameWithoutExtension => Path.GetFileNameWithoutExtension(this.FilePath);
-    public string Extension => Path.GetExtension(this.FilePath).TrimStart('.');
+    public string FileName => Path.GetFileName(FilePath);
+    public string FileNameWithoutExtension => Path.GetFileNameWithoutExtension(FilePath);
+    public string Extension => Path.GetExtension(FilePath).TrimStart('.');
 
     public CodeFile(string filePath)
     {
-        this.FilePath = filePath.OrFailIfWhiteSpace();
+        // TODO: Marcin Celej [from: Marcin Celej on: 26-07-2023]: fix contract check here
+        FilePath = filePath.OrFailIfWhiteSpace(nameof(filePath));
     }
 
-    public CodeFolder Folder 
-        => new(Path.GetDirectoryName(this.FilePath));
-    
+    public CodeFolder Folder
+        => new(Path.GetDirectoryName(FilePath)
+               ?? throw new ArgumentException("Invalid path", nameof(FilePath))
+        );
+
     public override string ToString()
-        => this.FilePath;
+        => FilePath;
 
     // public CodeFile RelativeTo(CodeFile file) 
     //     => new(Path.GetRelativePath(file.FilePath, FilePath));
 
     public CodeFile RelativeTo(CodeFolder folder)
-        => new(Path.GetRelativePath(folder.Path, this.FilePath));
+        => new(Path.GetRelativePath(folder.Path, FilePath));
 }
