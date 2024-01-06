5709d50b4de12c0af7e8fa7b94b7648373b7a82c
Marcin Celej <marcin.celej@gmail.com>
2024-01-06 21:28:18 +0100

    #27: Added [CodeFilePath] attribute

---
+++ Documentation/Synergy.Documentation.Annotations/CodeFilePathAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace Synergy.Documentation.Annotations;
+
+[Conditional("DOCUMENTATION")]
+public class CodeFilePathAttribute : Attribute
+{
+    public string FilePath { get; }
+
+    public CodeFilePathAttribute([CallerFilePath] string filePath = "")
+    {
+        FilePath = filePath;
+    }
+}
