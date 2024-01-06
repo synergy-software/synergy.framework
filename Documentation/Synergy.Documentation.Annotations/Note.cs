c9b07a41b396edbf9ae50c7b8e8d6404dfcd9251
Marcin Celej <marcin.celej@gmail.com>
2024-01-06 22:22:47 +0100

    #27: Add class Note to add comments into code as extensions

---
+++ Documentation/Synergy.Documentation.Annotations/Note.cs
@@ -0,0 +1,34 @@
+﻿namespace Synergy.Documentation.Annotations;
+
+public static class Note
+{
+    public static T Comment<T>(this T source, string comment)
+        => source;
+
+    public static T DoNothing<T>(this T source, string reason)
+        => source;
+
+    public static T DoNotThrowException<T>(this T source, string reason)
+        => source;
+
+    public static T Because<T>(this T source, string reason)
+        => source;
+
+    public static T Then<T>(this T source, string reason)
+        => source;
+
+    public static T But<T>(this T source, string reason)
+        => source;
+
+    public static T Therefore<T>(this T source, string reason)
+        => source;
+
+    public static T Otherwise<T>(this T source, string reason)
+        => source;
+
+    // public static Task<T> Otherwise<T>(this Task<T> source, string reason)
+    //     => source;
+
+    public static T Moreover<T>(this T source, string reason)
+        => source;
+}
--- Documentation/Synergy.Documentation.Tests/Architecture/Public/Api.of.Synergy.Documentation.Annotations.verified.md
+++ Documentation/Synergy.Documentation.Tests/Architecture/Public/Api.of.Synergy.Documentation.Annotations.verified.md
@@ -6,3 +6,41 @@
      filePath: string [CallerFilePath, Optional]
    )
 
+## Note (abstract class)
+ - Note.Because<T>(
+     source: T?,
+     reason: string
+   ) : T? [Extension]
+ - Note.But<T>(
+     source: T?,
+     reason: string
+   ) : T? [Extension]
+ - Note.Comment<T>(
+     source: T?,
+     comment: string
+   ) : T? [Extension]
+ - Note.DoNothing<T>(
+     source: T?,
+     reason: string
+   ) : T? [Extension]
+ - Note.DoNotThrowException<T>(
+     source: T?,
+     reason: string
+   ) : T? [Extension]
+ - Note.Moreover<T>(
+     source: T?,
+     reason: string
+   ) : T? [Extension]
+ - Note.Otherwise<T>(
+     source: T?,
+     reason: string
+   ) : T? [Extension]
+ - Note.Then<T>(
+     source: T?,
+     reason: string
+   ) : T? [Extension]
+ - Note.Therefore<T>(
+     source: T?,
+     reason: string
+   ) : T? [Extension]
+
