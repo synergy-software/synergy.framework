5e8bf1a0f496ad13ffee7c280b545da1d81d97ab
Marcin Celej <marcin.celej@gmail.com>
2024-01-08 19:25:14 +0100

    #27: trailing comment signs now are removed from TODO embedded in csproj file

--- Documentation/Synergy.Documentation/Todos/Patterns/CsprojTodoPattern.cs
+++ Documentation/Synergy.Documentation/Todos/Patterns/CsprojTodoPattern.cs
@@ -5,6 +5,5 @@ namespace Synergy.Documentation.Todos.Patterns;
 public record CsprojTodoPattern() : TodoPattern(
     "csproj",
     new Regex("(TODO.*)"),
-    match => match.Groups[1]
-                  .Value
+    match => match.Groups[1].Value.Replace("-->", "").Trim()
 );
