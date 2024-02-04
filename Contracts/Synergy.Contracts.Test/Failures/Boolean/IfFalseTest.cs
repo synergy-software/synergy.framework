83a5e1e4d5741812130a3fea140613d1a539807b
Marcin Celej <marcin.celej@gmail.com>
2024-02-04 12:39:06 +0100

    #28: Synergy.Contracts: Extracted dedicated test classes for boolean failures.

---
+++ Contracts/Synergy.Contracts.Test/Failures/Boolean/IfFalseTest.cs
@@ -0,0 +1,32 @@
+﻿using Xunit;
+using Assert = Xunit.Assert;
+
+namespace Synergy.Contracts.Test.Failures.Boolean;
+
+public class IfFalseTest
+{
+    [Fact]
+    public void IfFalseWithMessage()
+    {
+        // ARRANGE
+        bool someFalseValue = false;
+    
+        // ACT
+        var exception = Assert.Throws<DesignByContractViolationException>(
+            () => Fail.IfFalse(someFalseValue, Violation.Of("this should be true {0}", 1))
+        );
+    
+        // ASSERT
+        Assert.Equal("this should be true 1", exception.Message);
+    }
+
+    [Fact]
+    public void IfFalseSuccess()
+    {
+        // ARRANGE
+        var someTrueValue = true;
+
+        // ACT
+        Fail.IfFalse(someTrueValue, Violation.Of("this should be true"));
+    }
+}
---
+++ Contracts/Synergy.Contracts.Test/Failures/Boolean/IfTrueTest.cs
@@ -0,0 +1,31 @@
+﻿using Xunit;
+
+namespace Synergy.Contracts.Test.Failures.Boolean;
+
+public class IfTrueTest
+{
+    [Fact]
+    public void IfTrueWithMessage()
+    {
+        // ARRANGE
+        var someTrueValue = true;
+
+        // ACT
+        var exception = Assert.Throws<DesignByContractViolationException>(
+            () => Fail.IfTrue(someTrueValue, Violation.Of("this should be false {0}", 1))
+        );
+
+        // ASSERT
+        Assert.Equal("this should be false 1", exception.Message);
+    }
+        
+    [Fact]
+    public void IfTrueSuccess()
+    {
+        // ARRANGE
+        var someFalseValue = false;
+
+        // ACT
+        Fail.IfTrue(someFalseValue, Violation.Of("this should be false"));
+    }
+}
